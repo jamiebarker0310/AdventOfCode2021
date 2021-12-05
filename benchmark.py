9cebcee1fca7fda3fa8411468ec0f00ae359c562
Jamie <dodgerbarker@gmail.com>
2021-12-05 21:04:59 +0000

    linted benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -12,14 +12,16 @@ def benchmark(func: callable, file_path: str, N=5) -> list:
 
     return [time_function(func, file_path) for i in range(N)]
 
+
 def time_function(func: callable, file_path: str) -> float:
 
     start = time.time()
     func(file_path)
     return time.time() - start
 
+
 def get_modules():
-    modules =  [name for _, name, _ in pkgutil.iter_modules(['aoc'])]
+    modules = [name for _, name, _ in pkgutil.iter_modules(["aoc"])]
     module_dict = {}
     for module_str in modules:
         module = importlib.import_module(f"aoc.{module_str}", package=aoc)
@@ -30,6 +32,7 @@ def get_modules():
         module_dict[f"{module_str}-pt2"] = (func_2, file_path)
     return module_dict
 
+
 def run_benchmarks():
 
     module_dict = get_modules()
@@ -39,16 +42,16 @@ def run_benchmarks():
 
     return pd.DataFrame(module_dict)
 
+
 def save_figure(df):
 
-    df.mean().plot(figsize=(15,10), kind="bar")
+    df.mean().plot(figsize=(15, 10), kind="bar")
     plt.xticks(ticks=range(len(df.columns)), labels=df.columns, rotation=90)
     plt.ylabel("seconds")
     plt.yscale("log")
     return plt.savefig("benchmark.png")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     df = run_benchmarks()
     save_figure(df)
-
